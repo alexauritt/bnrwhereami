7eda45768039d6ee426d28e5bba9cb844284a97e
Alex Auritt <alexauritt@yahoo.com>
2013-01-11 14:52:25 -0500

    Initial Commit

---
+++ Whereami/WhereamiViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhereamiViewController.h
+//  Whereami
+//
+//  Created by Alexander Auritt on 1/11/13.
+//  Copyright (c) 2013 Alexander Auritt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WhereamiViewController : UIViewController
+
+@end
---
+++ Whereami/WhereamiViewController.m
@@ -0,0 +1,29 @@
+//
+//  WhereamiViewController.m
+//  Whereami
+//
+//  Created by Alexander Auritt on 1/11/13.
+//  Copyright (c) 2013 Alexander Auritt. All rights reserved.
+//
+
+#import "WhereamiViewController.h"
+
+@interface WhereamiViewController ()
+
+@end
+
+@implementation WhereamiViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
